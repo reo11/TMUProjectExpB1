1592de6a406e78eccd1302f12964508c6f64e64b
ReoDesktop <reohirao116@gmail.com>
2018-10-30 17:51:28 +0900

    answer.csvをsubmissionフォルダへ

--- mFile/querygen.m
+++ mFile/querygen.m
@@ -1,7 +1,8 @@
 Qpath = '../facedata/Query/jpeg/*.jpg';
 path = '../facedata/Query/jpeg/';
 D = dir(Qpath);
-fileID = fopen('answer.csv','w');
+fileID = fopen('../submission/answer.csv','w');
+
 str = {'query_name','answer'};
 fprintf(fileID,'query_num,answer\n');
 for i=1:length(D)
