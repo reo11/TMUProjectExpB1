a76ba90c9a367659915c21a84b108851c4c5bab6
ReoDesktop <reohirao116@gmail.com>
2018-10-30 17:50:04 +0900

    answer.csv作成

--- mFile/querygen.m
+++ mFile/querygen.m
@@ -1,15 +1,20 @@
 Qpath = '../facedata/Query/jpeg/*.jpg';
 path = '../facedata/Query/jpeg/';
 D = dir(Qpath);
+fileID = fopen('answer.csv','w');
+str = {'query_name','answer'};
+fprintf(fileID,'query_num,answer\n');
 for i=1:length(D)
    name = strcat(path, D(i).name);
    img = imread(name);
    answer = extractBefore(extractAfter(name,'jpeg/'),3);
     if answer(1) == 'r';
-        answer = -1;
-    else 
+        answer = 0;
+    else
         answer = str2num(answer) + 1;
     end
-   Query(:,:,i) = img;
-   QueryAnswer(i) = answer;
+    Query(:,:,i) = img;
+    QueryAnswer(i) = answer;
+    fprintf(fileID,'%s,%d\n',D(i).name ,answer);
 end
+fclose(fileID);
