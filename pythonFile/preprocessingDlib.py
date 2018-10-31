2c1505aad97020a75f5fc03fcb1358fc1f87fc11
ReoDesktop <reohirao116@gmail.com>
2018-10-31 18:51:45 +0900

    顔の特徴点データを追加

--- pythonFile/preprocessingDlib.py
+++ pythonFile/preprocessingDlib.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-import cv2
 import os
+import cv2
+import csv
 import dlib
 import imutils
 import numpy as np
+import pandas as pd
 from pathlib import Path
 from imutils import face_utils
 
@@ -12,6 +14,7 @@ QueryPath = '../dataset/Query/jpeg/'
 SIZE = (200, 200)
 
 FILENAME = 'DlibData'
+
 DBSavePath = '../' + FILENAME + '/DB/jpeg/'
 QuerySavePath = '../' + FILENAME + '/Query/jpeg/'
 DBPlotImagePath = '../' + FILENAME + '/DBPlot/jpeg/'
@@ -26,50 +29,37 @@ for path in [DBSavePath, QuerySavePath, DBPlotImagePath, \
 
 def cutFace():
     # Database
-    print('Start DB')
-    p = Path(DBPath)
-    p = sorted(p.glob("*.jpg"))
-    for filename in p:
-        img = cv2.imread(DBPath + filename.name)
-        ### frame is arranged image
-        ### roi is a part of face
-        frame, roi = face_shape_detector_dlib(img)
-        cv2.imshow('img', frame)
-        #cv2.imshow('img', roi)
-        cv2.waitKey(100)
-    print('Done DB')
+    # print('Start DB')
+    # p = Path(DBPath)
+    # p = sorted(p.glob("*.jpg"))
+    # df = pd.DataFrame()
+    # for filename in p:
+    #     img = cv2.imread(DBPath + filename.name)
+    #     face, plotImg, featurePoint_df = face_shape_detector_dlib(img, filename.name)
+    #     df = pd.concat([df, featurePoint_df])
+    #     cv2.imwrite(DBSavePath + filename.name, face)
+    #     cv2.imwrite(DBPlotImagePath + filename.name, plotImg)
+    #     print(featurePoint_df)
+    # df.to_csv(DBPlotCSVPath + 'featurePoint.csv')
+    # print('Done DB')
+
     print('Start Query')
     # Query
     p = Path(QueryPath)
     p = sorted(p.glob("*.jpg"))
+    df = pd.DataFrame()
     for filename in p:
         img = cv2.imread(QueryPath + filename.name)
-        for (x,y,w,h) in faces:
-            face = img[y:y+h, x:x+w]
-            face = preProcessing(face)
-        cv2.imwrite(QuerySavePath + filename.name,face)
+        face, plotImg, featurePoint_df = face_shape_detector_dlib(img, filename.name)
+        df = pd.concat([df, featurePoint_df])
+        cv2.imwrite(QuerySavePath + filename.name, face)
+        cv2.imwrite(QueryPlotImagePath + filename.name, plotImg)
+        print(featurePoint_df)
+    df.to_csv(QueryPlotCSVPath + 'featurePoint.csv')
     print('Done Query')
 
-def preProcessing(img):
-    img = cv2.resize(img, SIZE)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # #Sobelフィルタでx方向のエッジ検出
-    # gray_sobelx = cv2.Sobel(img,cv2.CV_32F,1,0)
-    # #Sobelフィルタでy方向のエッジ検出
-    # gray_sobely = cv2.Sobel(img,cv2.CV_32F,0,1)
-    # #8ビット符号なし整数変換
-    # gray_abs_sobelx = cv2.convertScaleAbs(gray_sobelx)
-    # gray_abs_sobely = cv2.convertScaleAbs(gray_sobely)
-    # #重み付き和
-    # img = cv2.addWeighted(gray_abs_sobelx,0.5,gray_abs_sobely,0.5,0)
-    # img = (img - np.mean(img))/np.std(img)*32+100
-    img = cv2.equalizeHist(img)
-    # img = cv2.Canny(img,100,200)
-    # img = cv2.GaussianBlur(img, (5, 5), 0)
-    return img
-
 # https://qiita.com/ufoo68/items/b1379b40ae6e63ed3c79
-def face_shape_detector_dlib(img):
+def face_shape_detector_dlib(img, name):
     # presetting
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     detector = dlib.get_frontal_face_detector()
@@ -79,9 +69,16 @@ def face_shape_detector_dlib(img):
     dets, scores, idx = detector.run(img_rgb, 0)
     if len(dets) > 0:
         for i, rect in enumerate(dets):
+            x = rect.left()
+            y = rect.top()
+            w = rect.width()
+            h = rect.height()
+            face = img[y:y+h, x:x+w]
+
             shape = predictor(img_rgb, rect)
             shape = face_utils.shape_to_np(shape)
             clone = img.copy()
+
             # cv2.putText(clone, "mouth", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             # landmarkを画像に書き込む
             # TODO
@@ -100,13 +97,43 @@ def face_shape_detector_dlib(img):
                 cv2.circle(clone, (x, y), 1, (255, 255, 0), -1)
             for (x, y) in shape[48:68]: # mouth
                 cv2.circle(clone, (x, y), 1, (0, 255, 0), -1)
+
+            df = pd.DataFrame()
+            for i, (x, y) in enumerate(shape[0:68]): # all
+                if i == 0:
+                    part = 'chin'
+                    count = 0
+                elif i == 17:
+                    part = 'right_eyebrow'
+                    count = 0
+                elif i == 22:
+                    part = 'left_eyebrow'
+                    count = 0
+                elif i == 27:
+                    part = 'nose'
+                    count = 0
+                elif i == 36:
+                    part = 'right_eye'
+                    count = 0
+                elif i == 42:
+                    part = 'left_eye'
+                    count = 0
+                elif i == 48:
+                    part = 'mouth'
+                    count = 0
+                xy_df = pd.DataFrame({
+                    part + '_x' + str(count) : [x],
+                    part + '_y' + str(count) : [y]},
+                    index = [name])
+                df = pd.concat([df, xy_df], axis=1)
+                count += 1
             # shapeで指定した個所の切り取り画像(ROI)を取得
-            (x, y, w, h) = cv2.boundingRect(np.array([shape[48:68]])) #口の部位のみ切り出し
-            roi = img[y:y + h, x:x + w]
-            roi = cv2.resize(roi,(100,100))
-        return clone, roi
+            # (x, y, w, h) = cv2.boundingRect(np.array([shape[48:68]])) #口の部位のみ切り出し
+            # roi = img[y:y + h, x:x + w]
+            # roi = cv2.resize(roi,(100,100))
+        return face, clone, df
     else :
-        return img, None
+        return img, img, None
 
 if __name__ == '__main__':
     cutFace()
