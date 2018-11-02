3ec0ead754365df9de94e98136cb8dfd06530c2b
ito-hiroki <i.hiroki0307@icloud.com>
2018-11-02 12:16:01 +0900

    DBの読み込み

---
+++ DlibDataChangeLuminace/make_feature.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 12:02:21 2018
+
+@author: hiroki
+"""
+
+import cv2
+import pandas as pd
+
+INPUT_DIR = 'DB/csv/'
+df = pd.read_csv(INPUT_DIR + "featurePoint.csv")
+
