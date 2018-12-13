609b237f7bd5dbbe412820fff7ce3bf360a50b90
Reo Hirao <reohirao116@gmail.com>
2018-12-13 17:01:14 +0900

    lightGBM dct

--- pythonFile/DCT.py
+++ pythonFile/DCT.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import cv2
 from scipy.fftpack import dct
 from pathlib import Path
@@ -20,74 +21,85 @@ QueryDctOriginalSavePath = '../dct/Query/dctOriginal/jpeg/'
 best_score = 0
 
 if __name__ == '__main__':
-    for i in tqdm(range(10,40)):
     #with open("dct.csv", mode='w') as f:
-        for j in range(i,i*3):
-            dbList = []
-            queryList = []
-            distanceList = []
-            answerList = []
-            count = 0
-            # print('c1: ' + str(i) + ', c2: ' + str(j))
-            #f.write('query_name,answer\n')
-            p = Path(dbPath)
-            p = sorted(p.glob("*.jpg"))
-            for filename in p:
-                img = cv2.imread(dbPath + filename.name, 0)
-                # cv2.imwrite(DBDctOriginalSavePath + filename.name, cv2.dct(np.float32(img)))
-                img = cv2.GaussianBlur(img, (7, 7), 2)
-                img = cv2.Canny(img, i, j)
-                # img = cv2.Canny(img, c1, c2)
-                # cv2.imwrite(DBCannySavePath + filename.name, img)
-                imf = np.float32(img)
-                dst = cv2.dct(imf)
-                # cv2.imwrite(DBDctSavePath + filename.name, dst)
-                dbList.append(np.float32(dst))
-            p = Path(queryPath)
-            p = sorted(p.glob("*.jpg"))
-            for filename in p:
-                distanceList = []
-                img = cv2.imread(queryPath + filename.name, 0)
-                # cv2.imwrite(QueryDctOriginalSavePath + filename.name, cv2.dct(np.float32(img)))
-                img = cv2.GaussianBlur(img, (7, 7), 2)
-                img = cv2.Canny(img, i, j)
-                # img = cv2.Canny(img, c1, c2)
-                # cv2.imwrite(QueryCannySavePath + filename.name, img)
-                imf = np.float32(img)
-                dst = cv2.dct(imf)
-                # cv2.imwrite(QueryDctSavePath + filename.name, dst)
-                queryList.append(np.float32(dst))
-                for index in range(len(dbList)):
-                    distance  = (dbList[index][:15, :15] - queryList[-1][:15, :15])**2
-                    distanceList.append(distance.sum())
-                distSort = np.argsort(distanceList)
-                answerList.append(np.uint8(np.argmin(distanceList)/10)+1)
-                ranking = np.uint8(distSort[:10]/10)+1
-                mode = np.argmax(np.bincount(ranking))
-                #print(str(ranking) + '   ' + str(answerList[-1]))
-                #print('mode: ' + str(mode))
-                # if mode != answerList[-1]:
-                target = filename.name[0:2]
-                if target[0] == 'r':
-                    target = 0
-                else:
-                    target = int(target) + 1
-                # 結果に応じて色を付ける
-                # if target == mode :
-                    #print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]), 'blue'))
-                if target == answerList[-1]:
-                    count += 1
-                #     print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'blue'))
-                # else:
-                #     print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'red'))
-                # distanceList = []
-                # f.write(filename.name + ',' + str(answerList[-1]) + '\n')
-            accuracy  = count / len(queryList) * 100
-            if best_score < accuracy:
-                best_score = accuracy
-                c1 = i
-                c2 = j
-                print('change Score: ' + str(int(best_score)) + 'c1: ' + str(c1) + ', c2: ' + str(c2))
-            #print(str(accuracy) + '%')
-    print(str(best_score) + '%')
-    print('c1: ' + str(c1) + ', c2: ' + str(c2))
+    dbList = []
+    queryList = []
+    distanceList = []
+    answerList = []
+    count = 0
+    # print('c1: ' + str(i) + ', c2: ' + str(j))
+    #f.write('query_name,answer\n')
+    p = Path(dbPath)
+    p = sorted(p.glob("*.jpg"))
+    feature_name_list = []
+    for i in range(225):
+        featrue_name = 'dct_' + str(i)
+        feature_name_list.append(featrue_name)
+    feature_name_list.append('target')
+    db_df = pd.DataFrame(columns=feature_name_list)
+    for index, filename in enumerate(tqdm(p)):
+        img = cv2.imread(dbPath + filename.name, 0)
+        # cv2.imwrite(DBDctOriginalSavePath + filename.name, cv2.dct(np.float32(img)))
+        img = cv2.GaussianBlur(img, (7, 7), 2)
+        img = cv2.Canny(img, 12, 34)
+        # img = cv2.Canny(img, c1, c2)
+        # cv2.imwrite(DBCannySavePath + filename.name, img)
+        imf = np.float32(img)
+        dst = cv2.dct(imf)
+        for ind, name in enumerate(feature_name_list):
+            db_df.loc[index, name] = np.array(dst).flatten()[ind]
+        db_df.loc[index, 'target'] = int(index/10) + 1
+        # cv2.imwrite(DBDctSavePath + filename.name, dst)
+        dbList.append(np.float32(dst))
+    db_df.to_csv("db_list.csv")
+    p = Path(queryPath)
+    p = sorted(p.glob("*.jpg"))
+    query_df = pd.DataFrame(columns=feature_name_list)
+    for index, filename in enumerate(tqdm(p)):
+        distanceList = []
+        img = cv2.imread(queryPath + filename.name, 0)
+        # cv2.imwrite(QueryDctOriginalSavePath + filename.name, cv2.dct(np.float32(img)))
+        img = cv2.GaussianBlur(img, (7, 7), 2)
+        img = cv2.Canny(img, 12, 34)
+        # img = cv2.Canny(img, c1, c2)
+        # cv2.imwrite(QueryCannySavePath + filename.name, img)
+        imf = np.float32(img)
+        dst = cv2.dct(imf)
+        for ind, name in enumerate(feature_name_list):
+            query_df.loc[index, name] = np.array(dst).flatten()[ind]
+        target = filename.name[0:2]
+        if target[0] == 'r':
+            target = 0
+        else:
+            target = int(target) + 1
+        query_df.loc[index, 'target'] = target
+        # cv2.imwrite(QueryDctSavePath + filename.name, dst)
+        queryList.append(np.float32(dst))
+        for index in range(len(dbList)):
+            distance  = (dbList[index][:15, :15] - queryList[-1][:15, :15])**2
+            distanceList.append(distance.sum())
+        distSort = np.argsort(distanceList)
+        answerList.append(np.uint8(np.argmin(distanceList)/10)+1)
+        ranking = np.uint8(distSort[:10]/10)+1
+        mode = np.argmax(np.bincount(ranking))
+        #print(str(ranking) + '   ' + str(answerList[-1]))
+        #print('mode: ' + str(mode))
+        # if mode != answerList[-1]:
+        target = filename.name[0:2]
+        if target[0] == 'r':
+            target = 0
+        else:
+            target = int(target) + 1
+        # 結果に応じて色を付ける
+        if target == mode :
+            print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]), 'blue'))
+        if target == answerList[-1]:
+            count += 1
+            print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'blue'))
+        else:
+            print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'red'))
+        distanceList = []
+        #f.write(filename.name + ',' + str(answerList[-1]) + '\n')
+    query_df.to_csv("query_list.csv")
+    accuracy  = count / len(queryList) * 100
+    print(str(accuracy) + '%')
--- pythonFile/lightGBM_BaseLine.py
+++ pythonFile/lightGBM_BaseLine.py
@@ -7,30 +7,15 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+DB_DATA_PATH = '../dct225/'
+QUERY_DATA_PATH = '../dct225/'
 
-# iris = load_iris()
-# X = iris.data
-# y = iris.target
-
-# X_train, X_test, y_train, y_test = train_test_split(
-#         X, y, test_size=0.2, random_state=0)
-# print(X_train)
-# print('\n')
-# print(X_test)
-# print('\n')
-# print(y_train)
-# print('\n')
-# print(y_test)
-
-DB_DATA_PATH = '../DlibDataChangeLuminace/DB/csv/'
-QUERY_DATA_PATH = '../DlibDataChangeLuminace/Query/csv/'
-
-X_train = pd.read_csv(DB_DATA_PATH + 'features_rel_dist.csv', index_col=0)
+X_train = pd.read_csv(DB_DATA_PATH + 'db_list.csv', index_col=0)
 y_train = X_train['target'].as_matrix()
 
 X_train = X_train.drop('target', axis=1).as_matrix()
-df = pd.read_csv(DB_DATA_PATH + 'features_rel_dist.csv', index_col=0)
-X_test = pd.read_csv(QUERY_DATA_PATH + 'features_rel_dist.csv', index_col=0)
+df = pd.read_csv(DB_DATA_PATH + 'db_list.csv', index_col=0)
+X_test = pd.read_csv(QUERY_DATA_PATH + 'query_list.csv', index_col=0)
 y_test = X_test['target'].as_matrix()
 
 X_test = X_test.drop('target', axis=1).as_matrix()
@@ -46,8 +31,8 @@ params = {
     'metric': {'multi_error'},
     'num_class': 21,
     'learning_rate': 0.01,
-    'num_leaves': 32,
-    'min_data_in_leaf': 2,
+    'num_leaves': 16,
+    'min_data_in_leaf': 10,
     'num_iteration': 200,
     'verbose': 20
 }
@@ -57,20 +42,19 @@ cols_to_drop = ['target']
 cols_to_fit = [col for col in df.columns if col not in cols_to_drop]
 gbm = lgb.train(params,
                 lgb_train,
-                num_boost_round=50,
+                num_boost_round=100,
                 valid_sets=lgb_eval,
-                early_stopping_rounds=100)
-y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
+                early_stopping_rounds=1000)
+y_pred = gbm.predict(X_test, num_iteration = gbm.best_iteration)
 y_pred_max = np.argmax(y_pred, axis=1)
 print(y_pred_max)
 print(y_test)
 
 feature_importance = pd.DataFrame(sorted(
     zip(gbm.feature_importance(), cols_to_fit)), columns=['Value', 'Feature'])
-
 plt.figure(figsize=(10, 6))
 sns.barplot(x="Value", y="Feature", data=feature_importance.sort_values(
-    by="Value", ascending=False))
+    by="Value", ascending=False).head(10))
 plt.title('LightGBM Features (avg over folds)')
 plt.tight_layout()
 plt.savefig('figure.png')
