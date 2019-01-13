460f8b8c3d8ea9647ce97da4a0dce8e3493d185b
Reo Hirao <reohirao116@gmail.com>
2019-01-13 21:28:07 +0900

    DCT作業中

--- pythonFile/DCT.py
+++ pythonFile/DCT.py
@@ -8,10 +8,10 @@ from termcolor import colored
 from tqdm import tqdm
 
 
-FILENAME = 'dataset2'
+FILENAME = '../input/Dlib/cutface/default'
 
-dbPath = '../' + FILENAME + '/DB/jpeg/'
-queryPath = '../' + FILENAME + '/Query/jpeg/'
+dbPath = FILENAME + '/DB/jpeg/'
+queryPath = FILENAME + '/Query/jpeg/'
 DBCannySavePath = '../dct/DB/canny/jpeg/'
 QueryCannySavePath = '../dct/Query/canny/jpeg/'
 DBDctSavePath = '../dct/DB/dctAfterCanny/jpeg/'
@@ -27,104 +27,113 @@ if __name__ == '__main__':
     distanceList = []
     answerList = []
     count = 0
-    # print('c1: ' + str(i) + ', c2: ' + str(j))
-    #f.write('query_name,answer\n')
-    p = Path(dbPath)
-    p = sorted(p.glob("*.jpg"))
-    feature_name_list = []
-    for i in range(225):
-        featrue_name = 'dct_' + str(i)
-        feature_name_list.append(featrue_name)
-    db_df = pd.DataFrame(columns=feature_name_list)
-    for index, filename in enumerate(tqdm(p)):
-        img = cv2.imread(dbPath + filename.name, 0)
-        # cv2.imwrite(DBDctOriginalSavePath + filename.name, cv2.dct(np.float32(img)))
-        img = cv2.GaussianBlur(img, (7, 7), 2)
-        img = cv2.Canny(img, 12, 34)
-        # img = cv2.Canny(img, c1, c2)
-        # cv2.imwrite(DBCannySavePath + filename.name, img)
-        imf = np.float32(img)
-        dst = cv2.dct(imf)
-        dst = dst[:15,:15]
-        for ind, name in enumerate(feature_name_list):
-            db_df.loc[index, name] = np.array(dst).flatten()[ind]
-        # cv2.imwrite(DBDctSavePath + filename.name, dst)
-        dbList.append(np.float32(dst))
-    p = Path(queryPath)
-    p = sorted(p.glob("*.jpg"))
-    query_df = pd.DataFrame(columns=feature_name_list)
-    for index, filename in enumerate(tqdm(p)):
-        distanceList = []
-        img = cv2.imread(queryPath + filename.name, 0)
-        # cv2.imwrite(QueryDctOriginalSavePath + filename.name, cv2.dct(np.float32(img)))
-        img = cv2.GaussianBlur(img, (7, 7), 2)
-        img = cv2.Canny(img, 12, 34)
-        # img = cv2.Canny(img, c1, c2)
-        # cv2.imwrite(QueryCannySavePath + filename.name, img)
-        imf = np.float32(img)
-        dst = cv2.dct(imf)
-        dst = dst[:15,:15]
-        for ind, name in enumerate(feature_name_list):
-            query_df.loc[index, name] = np.array(dst).flatten()[ind]
-        # cv2.imwrite(QueryDctSavePath + filename.name, dst)
-        queryList.append(np.float32(dst))
-        for index in range(len(dbList)):
-            distance  = (dbList[index][:15, :15] - queryList[-1][:15, :15])**2
-            distanceList.append(distance.sum())
-        distSort = np.argsort(distanceList)
-        answerList.append(np.uint8(np.argmin(distanceList)/10))
-        ranking = np.uint8(distSort[:10]/10)
-        mode = np.argmax(np.bincount(ranking))
-        #print(str(ranking) + '   ' + str(answerList[-1]))
-        #print('mode: ' + str(mode))
-        # if mode != answerList[-1]:
-        target = filename.name[0:2]
-        if target[0] == 'r':
-            target = 0
-        else:
-            target = int(target)
-        # 結果に応じて色を付ける
-        if target == mode :
-            print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]), 'blue'))
-        if target == answerList[-1]:
-            count += 1
-            print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'blue'))
-        else:
-            print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'red'))
-        distanceList = []
-        #f.write(filename.name + ',' + str(answerList[-1]) + '\n')
-    if db_df.min().min() < query_df.min().min():
-        norm_min = db_df.min().min()
-    else:
-        norm_min = query_df.min().min()
+    bestAcc = 0
+    for c1 in range(13,14):
+        for c2 in range(39,40):
+            print('c1: ' + str(c1) + ', c2: ' + str(c2))
+            #f.write('query_name,answer\n')
+            p = Path(dbPath)
+            p = sorted(p.glob("*.jpg"))
+            feature_name_list = []
+            for i in range(225):
+                featrue_name = 'dct_' + str(i)
+                feature_name_list.append(featrue_name)
+            db_df = pd.DataFrame(columns=feature_name_list)
+            for index, filename in enumerate(tqdm(p)):
+                img = cv2.imread(dbPath + filename.name, 0)
 
-    db_df = db_df - norm_min
-    query_df = query_df - norm_min
+                img = cv2.GaussianBlur(img, (7, 7), 2)
+                # img = cv2.Canny(img, 12, 34)
+                img = cv2.Canny(img, c1, c2)
 
-    if db_df.max().max() > query_df.max().max():
-        norm_max = db_df.max().max()
-    else:
-        norm_max = query_df.max().max()
+                imf = np.float32(img)
+                dst = cv2.dct(imf)
+                dst = dst[:15,:15]
+                for ind, name in enumerate(feature_name_list):
+                    db_df.loc[index, name] = np.array(dst).flatten()[ind]
 
-    db_df = db_df / norm_max
-    query_df = query_df / norm_max
+                dbList.append(np.float32(dst))
+            p = Path(queryPath)
+            p = sorted(p.glob("*.jpg"))
+            query_df = pd.DataFrame(columns=feature_name_list)
+            for index, filename in enumerate(tqdm(p)):
+                distanceList = []
+                img = cv2.imread(queryPath + filename.name, 0)
 
-    p = Path(dbPath)
-    p = sorted(p.glob("*.jpg"))
-    for index, filename in enumerate(tqdm(p)):
-        db_df.loc[index, 'target'] = int(index/10)
-    db_df.to_csv("db_list.csv")
+                img = cv2.GaussianBlur(img, (7, 7), 2)
+                # img = cv2.Canny(img, 12, 34)
+                img = cv2.Canny(img, c1, c2)
 
-    p = Path(queryPath)
-    p = sorted(p.glob("*.jpg"))
-    for index, filename in enumerate(tqdm(p)):
-        target = filename.name[0:2]
-        if target[0] == 'r':
-            target = 0
-        else:
-            target = int(target)
-        query_df.loc[index, 'target'] = target
-    query_df.to_csv("query_list.csv")
+                imf = np.float32(img)
+                dst = cv2.dct(imf)
+                dst = dst[:15,:15]
+                for ind, name in enumerate(feature_name_list):
+                    query_df.loc[index, name] = np.array(dst).flatten()[ind]
 
-    accuracy  = count / len(queryList) * 100
-    print(str(accuracy) + '%')
+                queryList.append(np.float32(dst))
+                for index in range(len(dbList)):
+                    distance  = (dbList[index][:15, :15] - queryList[-1][:15, :15])**2
+                    distanceList.append(distance.sum())
+                distSort = np.argsort(distanceList)
+                answerList.append(np.uint8(np.argmin(distanceList)/10))
+                ranking = np.uint8(distSort[:10]/10)
+                mode = np.argmax(np.bincount(ranking))
+
+                target = filename.name[0:2]
+                if target[0] == 'r':
+                    target = 20
+                else:
+                    target = int(target)
+                # 結果に応じて色を付ける
+                #if target == mode :
+                    #print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]), 'blue'))
+                if target == answerList[-1]:
+                    count += 1
+                    #print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'blue'))
+                #else:
+                    #print(colored(filename.name + ', mode: ' + str(mode) + ', answer: ' + str(answerList[-1]) + ', correct answer: ' + str(target), 'red'))
+                distanceList = []
+
+            # if db_df.min().min() < query_df.min().min():
+            #     norm_min = db_df.min().min()
+            # else:
+            #     norm_min = query_df.min().min()
+
+            # db_df = db_df - norm_min
+            # query_df = query_df - norm_min
+
+            # if db_df.max().max() > query_df.max().max():
+            #     norm_max = db_df.max().max()
+            # else:
+            #     norm_max = query_df.max().max()
+
+            # db_df = db_df / norm_max
+            # query_df = query_df / norm_max
+
+            # p = Path(dbPath)
+            # p = sorted(p.glob("*.jpg"))
+            # for index, filename in enumerate(tqdm(p)):
+            #     db_df.loc[index, 'target'] = int(index/10)
+            # db_df.to_csv("db_list.csv")
+
+            # p = Path(queryPath)
+            # p = sorted(p.glob("*.jpg"))
+            # for index, filename in enumerate(tqdm(p)):
+            #     target = filename.name[0:2]
+            #     if target[0] == 'r':
+            #         target = 0
+            #     else:
+            #         target = int(target)
+            #     query_df.loc[index, 'target'] = target
+            # query_df.to_csv("query_list.csv")
+
+            accuracy  = count / len(queryList) * 100
+            if accuracy > bestAcc:
+                bestAcc = accuracy
+                best_c1 = c1
+                best_c2 = c2
+                print('c1: ' + str(c1) + ', c2: ' + str(c2))
+                print('bestscore' + str(accuracy) + '%')
+            print(str(accuracy) + '%')
+    print('c1: ' + str(best_c1) + ', c2: ' + str(best_c2))
+    print('bestscore' + str(bestAcc) + '%')
